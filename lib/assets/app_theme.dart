0a0cd87a69c628b2809de170dd8b825fd59d8a5b
terromn <neil.rodriguez.murillo@gmail.com>
2023-10-03 13:01:36 -0600

    added a drawer and a logo to the app

---
+++ lib/assets/app_theme.dart
@@ -0,0 +1,189 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:outwork_mx_admin_app/utils/get_media_query.dart';
+
+import 'app_color_palette.dart';
+
+final String? _teFont = GoogleFonts.mPlusRounded1c().fontFamily;
+
+class TeAppThemeData {
+  static const double contentMargin = 32;
+
+  ThemeData getDarkTheme(BuildContext context) {
+    return ThemeData(
+      useMaterial3: true,
+      primaryColorDark: TeAppColorPalette.green,
+      progressIndicatorTheme: const ProgressIndicatorThemeData(
+        color: TeAppColorPalette.green,
+      ),
+
+      // APPBAR //
+      appBarTheme: AppBarTheme(
+        backgroundColor: TeAppColorPalette.green,
+        foregroundColor: TeAppColorPalette.black,
+        elevation: 12,
+        centerTitle: true,
+        titleTextStyle: TextStyle(
+          color: TeAppColorPalette.black,
+          fontSize: 26,
+          fontFamily: _teFont,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+
+      // DRAWER //
+      drawerTheme: const DrawerThemeData(
+        backgroundColor: TeAppColorPalette.blackLight,
+        elevation: 12,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            topRight: Radius.circular(30),
+            bottomRight: Radius.circular(30),
+          ),
+        ),
+      ),
+
+      // BOTTOM NAV BAR //
+      bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+        backgroundColor: TeAppColorPalette.green,
+        selectedItemColor: TeAppColorPalette.black,
+        showUnselectedLabels: false,
+        selectedIconTheme: IconThemeData(size: 42),
+        unselectedIconTheme: IconThemeData(size: 32),
+        elevation: 12,
+      ),
+
+      // ElEVATED BUTTON //
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ButtonStyle(
+          textStyle: MaterialStateProperty.all<TextStyle>(const TextStyle(
+            fontSize: 16.0,
+            fontWeight: FontWeight.bold,
+            color: TeAppColorPalette.black,
+          )),
+          padding: MaterialStateProperty.all<EdgeInsetsGeometry?>(
+              const EdgeInsets.symmetric(horizontal: 12, vertical: 4)),
+          backgroundColor:
+              MaterialStateProperty.all<Color>(TeAppColorPalette.green),
+          alignment: Alignment.center,
+          elevation: MaterialStateProperty.all(12),
+          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+          ),
+        ),
+      ),
+
+      // SNACK BAR //
+      snackBarTheme: const SnackBarThemeData(
+        backgroundColor:
+            TeAppColorPalette.green, // Set the desired background color
+        contentTextStyle: TextStyle(
+            color: TeAppColorPalette.black), // Set the desired text color
+      ),
+
+      // TEXT BUTTON //
+      textButtonTheme: TextButtonThemeData(
+        style: ButtonStyle(
+          foregroundColor: MaterialStateProperty.all<Color>(
+            TeAppColorPalette.black,
+          ),
+        ),
+      ),
+
+      // CARD //
+      cardTheme: CardTheme(
+        elevation: 24,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(100),
+        ),
+      ),
+
+      textSelectionTheme: const TextSelectionThemeData(
+        cursorColor: TeAppColorPalette.white,
+      ),
+
+      // TEXTFIELD //
+      inputDecorationTheme: const InputDecorationTheme(
+        labelStyle: TextStyle(color: TeAppColorPalette.white),
+        floatingLabelBehavior: FloatingLabelBehavior.never,
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: TeAppColorPalette.green, width: 2),
+          borderRadius: BorderRadius.all(Radius.circular(30)),
+        ),
+      ),
+
+      // TIME PICKER //
+      timePickerTheme: const TimePickerThemeData(
+        backgroundColor: TeAppColorPalette.black,
+        dialHandColor: TeAppColorPalette.green,
+        dayPeriodColor: TeAppColorPalette.green,
+
+        dialBackgroundColor: TeAppColorPalette.blackLight,
+        hourMinuteColor: TeAppColorPalette.blackLight,
+        dayPeriodTextColor: TeAppColorPalette.black,
+        hourMinuteTextColor: TeAppColorPalette.green,
+        // You can customize other properties as well
+      ),
+
+      datePickerTheme: DatePickerThemeData(
+        backgroundColor: TeAppColorPalette.black,
+        headerBackgroundColor: TeAppColorPalette.blackLight,
+        headerForegroundColor: TeAppColorPalette.white,
+        todayBorder: const BorderSide(color: TeAppColorPalette.green),
+        dividerColor: TeAppColorPalette.green,
+        todayBackgroundColor: MaterialStateProperty.all<Color>(
+          TeAppColorPalette.black,
+        ),
+        dayOverlayColor: MaterialStateProperty.all<Color>(
+          TeAppColorPalette.green,
+        ),
+        todayForegroundColor: MaterialStateProperty.all<Color>(
+          TeAppColorPalette.green,
+        ),
+        rangeSelectionOverlayColor: MaterialStateProperty.all<Color>(
+          TeAppColorPalette.green,
+        ),
+        rangePickerBackgroundColor: TeAppColorPalette.green,
+        dayBackgroundColor: MaterialStateProperty.resolveWith<Color>(
+            (Set<MaterialState> states) {
+          if (states.contains(MaterialState.selected)) {
+            return TeAppColorPalette.green;
+          } else {
+            return TeAppColorPalette.black;
+          }
+        }),
+        dayForegroundColor: MaterialStateProperty.resolveWith<Color>(
+            (Set<MaterialState> states) {
+          if (states.contains(MaterialState.selected)) {
+            return TeAppColorPalette.black;
+          } else {
+            return TeAppColorPalette.white;
+          }
+        }),
+        surfaceTintColor: TeAppColorPalette.green,
+        rangePickerHeaderForegroundColor: TeAppColorPalette.white,
+      ),
+
+      splashColor: TeAppColorPalette.green,
+      scaffoldBackgroundColor: TeAppColorPalette.blackLight,
+
+      // GENERAL //
+      brightness: Brightness.dark,
+      primaryColor: TeAppColorPalette.black,
+      fontFamily: _teFont,
+      textTheme: TextTheme(
+        displayLarge: const TextStyle(
+            fontSize: 32.0,
+            fontWeight: FontWeight.bold,
+            color: TeAppColorPalette.white),
+        displayMedium:
+            const TextStyle(fontSize: 22.0, fontWeight: FontWeight.bold),
+        bodyLarge: const TextStyle(
+            fontSize: 20.0,
+            color: TeAppColorPalette.white,
+            fontWeight: FontWeight.bold),
+        bodyMedium: TextStyle(fontSize: 14.0, color: Colors.grey[300]),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,73 +1,73 @@
 import 'package:flutter/material.dart';
+import 'package:outwork_mx_admin_app/assets/app_color_palette.dart';
+import 'package:outwork_mx_admin_app/assets/app_theme.dart';
+import 'package:outwork_mx_admin_app/utils/get_media_query.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MyHomePage(),
-         theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      
+      home: const MyHomePage(),
+      theme: TeAppThemeData().getDarkTheme(context),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Drawer Example'),
-      ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.blue,
+        body: Row(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            color: TeAppColorPalette.black,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.5),
+                spreadRadius: 5,
+                blurRadius: 10,
+                offset: const Offset(-4, 0),
               ),
-              child: Text('Drawer Header'),
+            ],
+            borderRadius: const BorderRadius.only(
+              topRight: Radius.circular(30),
+              bottomRight: Radius.circular(30),
             ),
-            ListTile(
-              title: Text('Item 1'),
-              onTap: () {
-                // Handle Item 1 tap
-              },
+          ),
+          width: TeMediaQuery.getPercentageWidth(context, 30),
+          child: Column(
+            
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+            
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: TeAppThemeData.contentMargin),
+              child: Image.asset(
+              'assets/outworkLogo.png', 
+              width: 200, 
+              height: 200, 
+                      ),
             ),
-            ListTile(
-              title: Text('Item 2'),
-              onTap: () {
-                // Handle Item 2 tap
-              },
-            ),
-          ],
+
+          ]),
         ),
-      ),
-      body: Center(
-        child: Text('Main Content'),
-      ),
-    );
+        Container(
+          decoration: const BoxDecoration(
+            color: TeAppColorPalette.blackLight,
+          ),
+          width: TeMediaQuery.getPercentageWidth(context, 70),
+        )
+      ],
+    ));
   }
 }
